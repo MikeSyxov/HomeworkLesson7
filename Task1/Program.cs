6acd82c5d2aeef7c67953d0e372c983208da3c11
Mikhail T <Mikola301@mail.ru>
2023-07-03 23:42:36 +0700

    added code into task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,48 @@
 // 0,5 7 -2 -0,2
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
+
+double[,] CreateArrD2(int rows, int cols)
+{
+    double[,] arrayD2 = new double[rows, cols];
+
+    for (int i = 0; i < arrayD2.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayD2.GetLength(1); j++)
+        {
+            arrayD2[i, j] = ((double)new Random().Next(-9, 9) * 1.2);   // заполнение вещественными числами
+            // if (arrayD2[i, j] == 0)
+            //     arrayD2[i, j] = arrayD2[i + 1, j + 1];
+        }
+    }
+    return arrayD2;
+}
+
+void ShowArrayD2(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]:F1}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    string inputedString = Console.ReadLine();
+    if (int.TryParse(inputedString, out int convertedInt))
+    {
+        return convertedInt;
+    }
+
+    System.Console.WriteLine("Вы ввели не число");
+    Environment.Exit(0);
+    return 0;
+}
+
+int rows = ReadInt("Введите число строк");
+int columns = ReadInt("Введите число столбцов");
+ShowArrayD2(CreateArrD2(rows, columns));
